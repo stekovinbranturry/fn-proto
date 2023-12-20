f10aacb18c33888f4c0a6337f9497530f64ee606
汉宣 <hanxuan.zk@alibaba-inc.com>
2023-12-20 21:36:15 +0800

    add home

---
+++ docs/Home.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <h1
+        style={{
+          margin: 64,
+          textAlign: 'center',
+        }}
+      >
+        使用 React 随便设计你的首页
+      </h1>
+      <img
+        style={{
+          display: 'block',
+          margin: '0 auto',
+        }}
+        width={800}
+        src="https://gw.alicdn.com/imgextra/i2/O1CN01mBMaqx1IkRdrqan4U_!!6000000000931-2-tps-4048-972.png"
+      />
+    </div>
+  );
+};
